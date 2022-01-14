377e2995dc481317c48ead58e93120f829b5047a
John Bannister <johnb@hey.com>
2022-01-14 09:22:29 +0000

    Add config for fish shell

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,6 @@
+# Setup homebrew variables and add installed binaries to PATH
+eval (/opt/homebrew/bin/brew shellenv)
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
