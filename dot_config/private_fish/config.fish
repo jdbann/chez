dc98cfd111828fc4fda4ce1ad8affb8424a2f2ae
John Bannister <johnb@hey.com>
2022-01-16 09:51:52 +0000

    Setup asdf on fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,9 @@
 # Setup homebrew variables and add installed binaries to PATH
 eval (/opt/homebrew/bin/brew shellenv)
 
+# Setup asdf variables
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
