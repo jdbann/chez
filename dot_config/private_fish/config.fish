3e206755c40df903e17353c65e9f369b83e7cbe5
John Bannister <johnb@hey.com>
2022-07-05 08:18:48 +0100

    Add direnv config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,6 +4,10 @@ eval (/opt/homebrew/bin/brew shellenv)
 # Setup asdf variables
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
+# Setup direnv
+set -g DIRENV_CONFIG /Users/johnbannister/.config/direnv
+direnv hook fish | source
+
 if status is-interactive
     # Setup gcloud completions
     source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
